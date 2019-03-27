905368d67f65d256dc50687dbbbe49bef824e923
Frikar <diegomilitar153@gmail.com>
2019-03-27 18:49:43 -0400

    comienzos de la inteligencia artificial

---
+++ src/IA/Computadora.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ia;
+
+import javax.swing.JLabel;
+import java.util.Random;
+
+/**
+ *
+ * @author Diego Vasquez
+ */
+public class Computadora extends game.gamemenu{
+   //Marcadores de Ganador o perdedor
+    public static javax.swing.JLabel lblnum1;
+    public static javax.swing.JLabel lblnum2;
+    public static javax.swing.JLabel lblcomputador;
+    //Funciones para el computador
+    public static int random;
+    private static double chancePapel = 33;
+    private static double chanceRoca = 33;
+    private static double repetirWin = 1;
+    private double repetirPerder = 1;
+    private double repetirEmpate = 1;
+    private double alterarWin = 0;
+    private double alterarPerder = 0;
+    private double alterarEmpate = 0;
+    private double conteoPapel = 0;
+    private double conteoRoca = 0;
+    private double conteoTijera = 0;
+    
+    //Inteligencia de la computadora
+    private double confianza = 4;
+    private double memoria = 3;
+
+  
+public static void roca(){
+
+}
+public static void papel(){
+    
+}
+public static void tijera(){
+    
+}
+public static void controlIA(){
+    
+}
+    }
+    
+    
+
