84e205a4af9136a844acb0cbf7209b746d393e9c
Frikar <diegomilitar153@gmail.com>
2019-03-27 22:26:37 -0400

    funciones para boton piedra

--- src/IA/Computadora.java
+++ src/IA/Computadora.java
@@ -12,43 +12,87 @@ import java.util.Random;
  *
  * @author Diego Vasquez
  */
-public class Computadora extends game.gamemenu{
-   //Marcadores de Ganador o perdedor
-    public static javax.swing.JLabel lblnum1;
-    public static javax.swing.JLabel lblnum2;
-    public static javax.swing.JLabel lblcomputador;
+public class Computadora extends game.gamemenu {
+
+    //Vistas
+    public static String usuarioselect, iaselect, resultado, historiaselect = "none";
+    public static int iapuntuaje = 0;
+    public static int usuariopuntuaje = 0;
+    public static int historiaresultado;
+
     //Funciones para el computador
     public static int random;
     private static double chancePapel = 33;
     private static double chanceRoca = 33;
     private static double repetirWin = 1;
-    private double repetirPerder = 1;
-    private double repetirEmpate = 1;
-    private double alterarWin = 0;
-    private double alterarPerder = 0;
-    private double alterarEmpate = 0;
-    private double conteoPapel = 0;
-    private double conteoRoca = 0;
-    private double conteoTijera = 0;
-    
+    private static double repetirPerder = 1;
+    private static double repetirEmpate = 1;
+    private static double alterarWin = 0;
+    private static double alterarPerder = 0;
+    private static double alterarEmpate = 0;
+    private static double conteoPapel = 0;
+    private static double conteoRoca = 0;
+    private static double conteoTijera = 0;
+
     //Inteligencia de la computadora
-    private double confianza = 4;
-    private double memoria = 3;
+    private static double confianza = 4;
+    private static double memoria = 3;
+    private static Random r = new Random();
 
-  
-public static void roca(){
+    public static void roca() {
+        usuarioselect = "Piedra";
 
-}
-public static void papel(){
-    
-}
-public static void tijera(){
-    
-}
-public static void controlIA(){
-    
-}
+        random = r.nextInt(102 - 1);
+
+        if (random < chancePapel) {
+            iaselect = "Papel";
+            iapuntuaje = iapuntuaje++;
+            confianza = confianza++;
+            resultado = "Perdiste";
+
+            historiaresultado = 0;
+            estrategia();
+            
+        } else {
+            
+            if (random < (chancePapel + chanceRoca)) {
+                iaselect = "Piedra";
+                resultado = "Empate";
+                estrategia();
+
+            }
+            else{
+              iaselect = "Tijera";
+              usuariopuntuaje = usuariopuntuaje++;
+              resultado = "Ganaste";
+              historiaresultado = 1;
+              estrategia();
+                
+            }
+        }
     }
-    
-    
 
+    public static void papel() {
+        usuarioselect = "Papel";
+    }
+
+    public static void tijera() {
+        usuarioselect = "Tijera";
+    }
+
+    public static void controlIA() {
+
+    }
+
+    public static void aprendizajeIA() {
+
+    }
+
+    public static void estrategia() {
+        if (historiaselect.equals(usuarioselect)) {
+            repetirPerder = repetirPerder++;
+        } else {
+            alterarWin = alterarWin++;
+        }
+    }
+}
